71305a462e34b226593fff5eb0b8288b253d2ae1
LihasSahil <sahilsindhu14@gmail.com>
2022-03-30 01:15:55 -0400

    new changes class6

--- structs.go
+++ structs.go
@@ -2,67 +2,58 @@ package main
 
 import (
 	"fmt"
-	"os"
 )
 
-type Overview struct {
+var IE string
+var wmt int = 0
+var dmt int = 0
+
+type AccDetail struct {
 	Name   string
-	UserId int
-	Funds  bal
+	Accno  int
+	Funds  FDT
 	AcType string
 }
 
-func (o *Overview) ChangeAcType(newAcType string) {
-	o.AcType = newAcType
+type FDT struct {
+	Balance  int
+	Withdraw int
+	Deposit  int
 }
 
-type bal struct {
-	Balance    int
-	Withdrawal int
-	Deposit    int
+type Bank interface {
+	AddFunds()
+	GetAccountInfo()
+	Withdrawfunds()
 }
 
+func (a *AccDetail) GetAccountInfo() {
+	fmt.Println("name:", a.Name, "Type: ", a.AcType, "Balance:", a.Funds, "A/c no:", a.Accno)
+}
+func (f *FDT) WithdrawFunds() {
+	fmt.Println(" withdraw amount: ")
+	fmt.Scanln(&wmt)
+	fmt.Println("your remaining balance", f.Balance-wmt)
+}
+func (f *FDT) AddFunds() {
+	fmt.Println(" deposit amount: ")
+	fmt.Scanln(&dmt)
+	fmt.Println("your remaining balance", f.Balance+dmt)
+}
 func structs() {
-	var UserNo int
-	var TFunc string
-	var Wamount int = 0
-	var Damount int = 0
 
-	fmt.Println("enter UserNo:")
-	fmt.Scanln(&UserNo)
-
-	if UserNo == 20202020 {
-		var John = Overview{Name: "John Smith", UserId: 20202020, Funds: bal{Balance: 50000, Withdrawal: 00, Deposit: 0}, AcType: "savings"}
-		fmt.Printf("%+v \n", John)
-
-		Newbal := bal{
-			Balance:    50000,
-			Withdrawal: 0,
-			Deposit:    0,
-		}
-		fmt.Println("balance is :", Newbal.Balance-Newbal.Withdrawal+Newbal.Deposit)
-		John.ChangeAcType("checking")
-		fmt.Printf("%+v \n", John)
-		fmt.Println("would you like to perform a transaction : (W)Withdrawal, Deposit(D),(B) checkBalance:")
-		fmt.Scanln(&TFunc)
-		switch TFunc {
-		case "W":
-			fmt.Println("amount:")
-			fmt.Scanln(&Wamount)
-			fmt.Println("balance is :", Newbal.Balance-Wamount+Newbal.Deposit)
-		case "D":
-			fmt.Println("amount:")
-			fmt.Scanln(&Damount)
-			fmt.Println("balance is :", Newbal.Balance-Newbal.Withdrawal+Damount)
-
-		case "B":
-			fmt.Println("balance is :", Newbal.Balance-Wamount+Newbal.Deposit)
-
-		default:
-			fmt.Println("invalid option")
-		}
-	} else {
-		fmt.Println("Wrong UserID")
-		os.Exit(3)
+	var John = AccDetail{Name: "John Smith", Accno: 20202020, Funds: FDT{Balance: 50000, Withdraw: 0, Deposit: 0}, AcType: "savings"}
+	fmt.Printf("%+v \n", John)
+	fmt.Printf("Enter Interface (WithdrawFunds,AddFunds,GetAccountInfo): ")
+	fmt.Scanln(&IE)
+	switch IE {
+	case "GetAccountInfo":
+		John.GetAccountInfo()
+	case "WithdrawFunds":
+		John.Funds.WithdrawFunds()
+	case "AddFunds":
+		John.Funds.AddFunds()
+	default:
+		fmt.Println("invalid input")
 	}
 }
